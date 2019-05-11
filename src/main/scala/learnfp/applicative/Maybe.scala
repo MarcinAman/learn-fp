8570e134f89eb1cafac57c5a687185e5e9a3797d
MarcinAman <marcin.aman@gmail.com>
2019-05-11 12:15:57 +0200

    asd

--- src/main/scala/learnfp/applicative/Maybe.scala
+++ src/main/scala/learnfp/applicative/Maybe.scala
@@ -1,16 +1,18 @@
 package learnfp.applicative
 
-import learnfp.functor.Maybe.Maybe
+import learnfp.functor.Maybe.{Just, Maybe, Nothing}
 import learnfp.functor.{MaybeInstance => MaybeFunctorInstance}
-
-import learnfp.functor.Maybe.{Maybe, Just, Nothing}
 
 
 object MaybeInstance {
   import MaybeFunctorInstance._
-  import learnfp.functor.FunctorOps._
   implicit val maybeApplicativeInstance = new Applicative[Maybe] {
-    override def pure[A](a: A): Maybe[A] = ???
-    override def <*>[A, R](fx: Maybe[A => R])(a: Maybe[A]): Maybe[R] = ???
+    override def pure[A](a: A): Maybe[A] = Just(a)
+    override def <*>[A, R](fx: Maybe[A => R])(a: Maybe[A]): Maybe[R] = {
+      (a, fx) match {
+        case (Just(v), Just(f)) => Just(f(v))
+        case _ => Nothing()
+      }
+    }
   }
 }
