c112d4c2c37612e524e01432e48443fce95cbdef
Marcin Aman <marcin.aman@comarch.pl>
2019-06-21 16:54:40 +0200

    .

--- src/main/scala/learnfp/monad/Writer.scala
+++ src/main/scala/learnfp/monad/Writer.scala
@@ -3,12 +3,16 @@ package learnfp.monad
 import learnfp.functor.Writer
 import learnfp.functor.WriterInstance._
 import learnfp.monoid.Monoid
-import learnfp.monoid.MonoidOps._
 
 object WriterInstance {
   implicit def writerMonadInstance[W](implicit monoid:Monoid[W]) = new Monad[({type E[X] = Writer[W, X]})#E] {
-    override def pure[A](a: A): Writer[W, A] = ???
-    override def flatMap[A, B](a: Writer[W, A])(fx: A => Writer[W, B]): Writer[W, B] = ???
+    override def pure[A](a: A): Writer[W, A] = Writer(() => (monoid.mzero, a))
+    override def flatMap[A, B](a: Writer[W, A])(fx: A => Writer[W, B]): Writer[W, B] = {
+      val (aw, aa) = a.run()
+      val (fxw, fxa) = fx(aa).run()
+
+      Writer(() => (monoid.mappend(aw, fxw), fxa))
+    }
   }
 
   implicit def writerToMonadOps[W, A](w:Writer[W, A])(implicit monoid:Monoid[W]) = new MonadOps[A, ({type E[X] = Writer[W, X]})#E](w)
